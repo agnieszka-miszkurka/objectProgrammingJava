3988d4586b3c3be5fd84ca1911fa6c6fc4ad5d4b
Agnieszka Miszkurka <miszkur@mops.uci.agh.edu.pl>
2017-10-26 12:49:36 +0200

    initial commit

---
+++ lab1/Main.java
@@ -0,0 +1,15 @@
+package lab1;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Main {
+	public static void main(String[] args){
+		System.out.println("Hello world");
+		Scanner scan = new Scanner(System.in);
+		String s = scan.next();
+		int i = scan.nextInt();
+		double d = scan.nextDouble();
+		System.out.printf("Wczytano %s , %d, %f",s,i,d);	
+	}
+}
---
+++ lab1/lab1/src/lab1/Problem115A.java
@@ -0,0 +1,7 @@
+package lab1;
+
+public class Problem115A {
+    public static void main(String [] args){
+
+    }
+}
