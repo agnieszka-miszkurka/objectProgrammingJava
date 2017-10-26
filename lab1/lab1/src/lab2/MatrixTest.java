213e425568824b9a9bbece554e792d83c3310e57
agnieszka.miszkurka <kama337>
2017-10-26 13:38:06 +0200

    commit from intellij

---
+++ lab1/lab1/src/lab2/MatrixTest.java
@@ -0,0 +1,67 @@
+package lab2;
+
+import static org.junit.Assert.*;
+
+public class MatrixTest {
+    @org.junit.Test
+    public void getRows() throws Exception {
+        ooo
+    }
+
+    @org.junit.Test
+    public void getCols() throws Exception {
+    }
+
+    @org.junit.Test
+    public void get() throws Exception {
+    }
+
+    @org.junit.Test
+    public void set() throws Exception {
+    }
+
+    @org.junit.Test
+    public void asArray() throws Exception {
+    }
+
+    @org.junit.Test
+    public void testToString() throws Exception {
+    }
+
+    @org.junit.Test
+    public void reshape() throws Exception {
+    }
+
+    @org.junit.Test
+    public void shape() throws Exception {
+    }
+
+    @org.junit.Test
+    public void add() throws Exception {
+    }
+
+    @org.junit.Test
+    public void sub() throws Exception {
+    }
+
+    @org.junit.Test
+    public void mul() throws Exception {
+    }
+
+    @org.junit.Test
+    public void div() throws Exception {
+    }
+
+    @org.junit.Test
+    public void dot() throws Exception {
+    }
+
+    @org.junit.Test
+    public void frobenius() throws Exception {
+    }
+
+    @org.junit.Test
+    public void main() throws Exception {
+    }
+
+}
